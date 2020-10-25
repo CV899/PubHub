568ca9b6236ebcb7a6a1be3d7c154aa562289698
Christian Vincent <christian.vincent25@hotmail.com>
2020-10-25 18:21:03 -0400

    Adds a POJO for book tags

--- PubHub/src/examples/pubhub/model/Tag.java
+++ PubHub/src/examples/pubhub/model/Tag.java
@@ -1,5 +1,23 @@
 package examples.pubhub.model;
 
 public class Tag {
-
+	
+	private String tagName;
+	
+	public Tag() {
+		tagName = null;
+	}
+	
+	public Tag(String tagName) {
+		this.tagName = tagName;
+	}
+	
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+	
+	public String getTagName() {
+		return tagName;
+	}
 }
+
