3bf5d339f2ce5b223501b52bf9cf9618d35ca2c4
Christian Vincent <christian.vincent25@hotmail.com>
2020-10-27 12:03:49 -0400

    BookTagDAO Interface revision
    Replaces primitive arguments (String, String) with POJOs that represents
    books and book tags

--- PubHub/src/examples/pubhub/dao/BookTagDAO.java
+++ PubHub/src/examples/pubhub/dao/BookTagDAO.java
@@ -2,13 +2,14 @@ package examples.pubhub.dao;
 import java.util.*;
 
 import examples.pubhub.model.Book;
+import examples.pubhub.model.Tag;
 /**
  * Interface for our Data Access Object to handle database queries related to book tags.
  */
 public interface BookTagDAO {
-	public boolean addTag(String isbn, String tagName);
-	public boolean removeTag(String isbn, String tagName);
+	public boolean addTag(Book book, Tag tag);
+	public boolean removeTag(Book book, Tag tag);
 	
-	public List<String> getTags(String isbn);
-	public List<Book> getBooksWithTag(String tagName);
+	public List<String> getTags(Tag tag);
+	public List<Book> getBooksWithTag(Tag tag);
 }
